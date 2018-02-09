27d861d319dbbd51549809dde46ef2a2025aa966
codes-kzhan <kzhan@lzu.edu.cn>
2018-02-09 19:20:49 +1100

    Update nii2npy.py

--- DATA2NPY/nii2npy.py
+++ DATA2NPY/nii2npy.py
@@ -13,14 +13,14 @@ if not os.path.exists(path2):
 
 for n in range(N):
     volumeID = '{:0>4}'.format(n + 1)
-    print 'Processing File ' + volumeID
+    print('Processing File ' + volumeID)
     filename1 = 'label' + volumeID + '.nii.gz'
     directory1 = os.path.join(path1, filename1)
     filename2 = volumeID + '.npy'
     file1 = os.path.join(path1, filename1)
     data = nibabel.load(file1).get_data().transpose(1, 0, 2)
-    print '  Data shape is ' + str(data.shape) + ' .'
+    print('  Data shape is ' + str(data.shape) + ' .')
     file2 = os.path.join(path2, filename2)
     np.save(file2, data)
-    print 'File ' + volumeID + ' is saved in ' + file2 + ' .'
+    print('File ' + volumeID + ' is saved in ' + file2 + ' .')
 
