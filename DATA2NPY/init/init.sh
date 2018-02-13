5ea3d64f2e2f7d395829b13171794cdb29826f21
codes-kzhan <kzhan@lzu.edu.cn>
2018-02-13 11:56:14 +1100

    Add files via upload

---
+++ DATA2NPY/init/init.sh
@@ -0,0 +1,26 @@
+
+####################################################################################################
+# defining the root path which stores image and label data
+ENABLE_INITIALIZATION=1
+
+DATA_PATH='/home/zhankun/Pancreas/dataset'
+
+####################################################################################################
+# export LD_LIBRARY_PATH and PYTHONPATH (related to your path to CUDA and CAFFE)
+#export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+#export PYTHONPATH=${DATA_PATH}libs/caffe-master/python:$PYTHONPATH
+
+####################################################################################################
+
+####################################################################################################
+# data initialization: only needs to be run once
+# variables
+ORGAN_NUMBER=1
+FOLDS=4
+LOW_RANGE=-100
+HIGH_RANGE=240
+if [ "$ENABLE_INITIALIZATION" = "1" ]
+then
+    python init.py \
+        $DATA_PATH $ORGAN_NUMBER $FOLDS $LOW_RANGE $HIGH_RANGE
+fi
